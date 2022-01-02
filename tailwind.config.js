5513bf064bfdb7d3f0de73f77538c73a51f99bc8
Justyna Marciniak <justy.marciniak@gmail.com>
2022-01-02 17:52:21 +0900

    create navbar

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Nunito'],
+      },
+      colors: {
+        sand: {
+          100: '#FFFFFF',
+          200: '#FFFCF1',
+        },
+        coral: '#FDDACF',
+        peach: '#F78E72',
+        green: '#1E8079',
+      },
+    },
+  },
+  plugins: [],
+}
