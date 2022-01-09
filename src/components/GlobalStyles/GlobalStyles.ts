aea47a00ca84491de6e9aa6ba47beab4f1676232
Justyna Marciniak <justy.marciniak@gmail.com>
2022-01-09 15:02:25 +0900

    add globalStyles

---
+++ src/components/GlobalStyles/GlobalStyles.ts
@@ -0,0 +1,31 @@
+import tw from 'tailwind-styled-components'
+
+export const ContainerWrapper = tw.div`
+  container
+  mx-auto
+`
+export const TitleWrapper = tw.div`
+  px-2
+`
+export const Title = tw.h1`
+  text-2xl md:text-4xl font-bold text-center text-peach-200 p-6 pt-20 font-body uppercase
+`
+export const Subtitle = tw.p`
+  text-green text-center pb-6
+`
+export const ProductsWrapper = tw.div`
+  grid md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10 py-12 justify-center md:px-3
+`
+export const PageTitleWrapper = tw.div`
+  mt-20 h-max
+`
+
+export const OrangeButton = tw.button`
+text-sand-100
+bg-peach-100
+hover:bg-peach-200
+  p-3
+  px-4
+  cursor-pointer
+  shadow
+`
