894027bbcd6fe39275d24209addbb823119db382
Justyna Marciniak <justy.marciniak@gmail.com>
2022-01-20 07:52:20 +0900

    create style file for ProductCart

--- src/components/Cart/ProductCart/ProductCart.tsx
+++ src/components/Cart/ProductCart/ProductCart.tsx
@@ -3,7 +3,19 @@ import { useNavigate } from 'react-router-dom'
 import CartItem from '../CartItem/CartItem'
 import { useAppSelector } from '../../../hooks/hooks'
 import { OrangeButton } from '../../GlobalStyles/GlobalStyles'
-
+import {
+  ProductListWrapper,
+  ProductListTable,
+  ProductListTableHead,
+  ProductListTableRow,
+  FirstHeaderCell,
+  HeaderCell,
+  TableBody,
+  CartTotal,
+  CartTitle,
+  CartTotalPrice,
+  ButtonWrapper,
+} from './ProductCartStyles'
 const ProductCart = () => {
   const products = useAppSelector((state) => state.cart.cartItems)
   const totalPrice = useAppSelector((state) => state.cart.totalPrice)
@@ -15,47 +27,35 @@ const ProductCart = () => {
   }
 
   return (
-    <div>
-      <div className='my-10 px-2'>
-        <table className='table-fixed border-collapse w-full text-center'>
-          <thead className=''>
-            <tr className='border-b border-ash'>
-              <th className='w-0 p-3 uppercase tracking-widest md:w-2'></th>
-              <th className='text-tiny md:text-base p-3 uppercase tracking-widest'>
-                Product
-              </th>
-              <th className='text-tiny md:text-base p-3 uppercase tracking-widest'>
-                Price
-              </th>
-              <th className='text-tiny md:text-base p-3 uppercase tracking-widest'>
-                Quantity
-              </th>
-              <th className='text-tiny md:text-base p-3 uppercase tracking-widest'>
-                Subtotal
-              </th>
-            </tr>
-          </thead>
-          <tbody>
+    <>
+      <ProductListWrapper>
+        <ProductListTable>
+          <ProductListTableHead>
+            <ProductListTableRow>
+              <FirstHeaderCell></FirstHeaderCell>
+              <HeaderCell>Product</HeaderCell>
+              <HeaderCell>Price</HeaderCell>
+              <HeaderCell>Quantity</HeaderCell>
+              <HeaderCell>Subtotal</HeaderCell>
+            </ProductListTableRow>
+          </ProductListTableHead>
+          <TableBody>
             {products.map((product) => (
               <CartItem key={product.id} product={product} />
             ))}
-          </tbody>
-        </table>
-      </div>
-      <div className='grid md:grid-cols-2 p-10 md:p-20 '>
-        <h3 className='col-span-1 text-center md:text-left uppercase text-peach-200 font-bold text-xl md:text-2xl'>
-          Cart Total
-        </h3>
-        <h3 className='col-span-1 text-center text-xl md:text-2xl font-bold'>
-          ${totalPrice.toFixed(2)}
-        </h3>
-      </div>
-      <div className='text-center mb-28'>
+          </TableBody>
+        </ProductListTable>
+      </ProductListWrapper>
+      <CartTotal>
+        <CartTitle>Cart Total</CartTitle>
+        <CartTotalPrice>${totalPrice.toFixed(2)}</CartTotalPrice>
+      </CartTotal>
+      <ButtonWrapper>
         <OrangeButton onClick={handleBackToProductList}>
           Back to product list
         </OrangeButton>
-      </div>
-    </div>
+      </ButtonWrapper>
+    </>
   )
 }
 
---
+++ src/components/Cart/ProductCart/ProductCartStyles.ts
@@ -0,0 +1,35 @@
+import tw from 'tailwind-styled-components'
+
+export const ProductListWrapper = tw.div`
+  my-10 px-2
+`
+export const ProductListTable = tw.table`
+  table-fixed border-collapse w-full text-center
+`
+export const ProductListTableHead = tw.thead`
+  border-b border-ash
+`
+export const ProductListTableRow = tw.tr`
+  border-b border-ash
+`
+export const FirstHeaderCell = tw.th`
+  w-0 p-3 uppercase tracking-widest md:w-2
+`
+export const HeaderCell = tw.th`
+  text-tiny md:text-base p-3 uppercase tracking-widest
+`
+export const TableBody = tw.tbody`
+
+`
+export const CartTotal = tw.div`
+  grid md:grid-cols-2 p-10 md:p-20
+`
+export const CartTitle = tw.h3`
+  col-span-1 text-center md:text-left uppercase text-peach-200 font-bold text-xl md:text-2xl
+`
+export const CartTotalPrice = tw.h3`
+  col-span-1 text-center text-xl md:text-2xl font-bold
+`
+export const ButtonWrapper = tw.div`
+  text-center mb-28
+`
