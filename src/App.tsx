19ee8dc54a0219c9ea14c4d3d1fd61d09a69ad0b
Justyna Marciniak <justy.marciniak@gmail.com>
2022-02-19 21:52:57 +0900

    add hashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import './AppStyles.tsx'
 
@@ -15,7 +15,7 @@ function App() {
     <>
       <Layout>
         <Routes>
-          <Route path='/' element={<Home />}></Route>
+          <Route path='/makeup-store' element={<Home />}></Route>
           <Route path='/products' element={<AllProducts />}></Route>
           <Route path='/products/:id' element={<SingleProduct />}></Route>
           <Route path='/contact' element={<Contact />}></Route>
